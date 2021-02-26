5b35d19a63c8d1cbbb76f2cb3aaddc65bfda3a08
Slavik Shudenko <shudenkoslavik@gmail.com>
2021-02-26 21:23:24 +0200

    Remove ButtonDrawer from root path

--- ButtonDrawer.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEditor;
-using UnityEngine;
-
-[CustomEditor(typeof(MonoBehaviour))]
-public class ButtonDrawer : Editor {
-    public override void OnInspectorGUI() {
-        base.OnInspectorGUI();
-    }
-}
