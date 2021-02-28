f1849de625bc7b0a279987113c86c2b82009f483
Slavik Shudenko <shudenkoslavik@gmail.com>
2021-03-01 00:21:20 +0200

    Update scene, utitiles, some comments

--- Assets/Scripts/Utils/GuiUtilities.cs
+++ Assets/Scripts/Utils/GuiUtilities.cs
@@ -1,15 +1,21 @@
 ﻿namespace Assets.Scripts.Utils {
     using System.Reflection;
     using Attributes;
+    using UnityEditor;
     using UnityEngine;
 
     public class GuiUtilities {
         public static void Button(Object obj, MethodInfo info) {
             var attr = (ButtonAttribute)info.GetCustomAttributes(typeof(ButtonAttribute), true)[0];
-
+            
             if (GUILayout.Button(attr.Text)) {
                 info.Invoke(obj, new object[]{ });
             }
+        }
+
+        public static void Dropdown(Object obj, FieldInfo info) {
+            var attr = (DropdownAttribute) info.GetCustomAttributes(typeof(DropdownAttribute), true)[0];
+            //Todo: check & refactor
         }
     }
 }
--- Assets/TestBehaviour.cs
+++ Assets/TestBehaviour.cs
@@ -5,14 +5,19 @@
 
     public class TestBehaviour : MonoBehaviour {
 
-        [Button("Simple button to click")]
+        [Dropdown("intValues")]
+        public int intValue;
+
+        private int[] intValues = {1, 2, 3, 4, 5}; 
+
+        [Button("Simple button to click one")]
         public void SimpleLog() {
             Debug.Log("Simple log");
         }
         
         [Button]
         public void SimpleLogWithoutName() {
-            Debug.Log("Simple log2");
+            Debug.Log("Simple log two");
         }
     }
 }
