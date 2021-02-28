c02a41f7957123706551c3a77a0f47c65b79f2aa
Slavik Shudenko <shudenkoslavik@gmail.com>
2021-03-01 00:20:37 +0200

    Create dropdown attribute

---
+++ Assets/Scripts/Attributes/DropdownAttribute.cs
@@ -0,0 +1,14 @@
+﻿namespace Assets.Scripts.Attributes {
+    using System;
+    using UnityEngine;
+
+    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property)]
+    public class DropdownAttribute : PropertyAttribute, IAttribute {
+
+        public string Name { get; private set; }
+
+        public DropdownAttribute(string name) {
+            Name = name;
+        }
+    }
+}
---
+++ Assets/Scripts/Editor/PropertyDrawers/DropdownDrawer.cs
@@ -0,0 +1,20 @@
+﻿namespace Assets.Scripts.Editor.PropertyDrawers {
+    using Attributes;
+    using UnityEditor;
+    using UnityEngine;
+
+    [CustomPropertyDrawer(typeof(DropdownAttribute))]
+    public class DropdownDrawer : BaseDrawer {
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label) {
+            EditorGUI.BeginProperty(position, label, property);
+            
+            position = EditorGUI.PrefixLabel(position, GUIUtility.GetControlID(FocusType.Passive), label);
+            
+            EditorGUI.PropertyField(position, property, label, true);
+            EditorGUI.EndProperty();
+            
+            //Here we have to add an implementation of DropdownDrawer using reflection to get linked data (e.g property.name)
+            //Then, we could use a custom method to draw a Dropdown. (Utils)
+        }
+    }
+}
