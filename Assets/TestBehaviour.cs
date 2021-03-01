273594c1b49acf376818fc3faf6d45030a2de196
Slavik Shudenko <shudenkoslavik@gmail.com>
2021-03-01 19:58:50 +0200

    Update test script

--- Assets/TestBehaviour.cs
+++ Assets/TestBehaviour.cs
@@ -1,6 +1,7 @@
 ﻿namespace Assets {
     using System.Collections.Generic;
     using Scripts.Attributes;
+    using Scripts.Utils;
     using UnityEngine;
 
     public class TestBehaviour : MonoBehaviour {
@@ -8,8 +9,18 @@
         [Dropdown("intValues")]
         public int intValue;
 
-        private int[] intValues = {1, 2, 3, 4, 5}; 
+        private int[] intValues = {1, 2, 3, 4, 5};
 
+        [Dropdown("vectorValues")]
+        private Vector3 vectorValue;
+
+        private DropdownList<string> vectorValues = new DropdownList<string>() {
+            {"test", "Testcaseone"},
+            {"testtwo", "Testcasetwo"},
+            {"testthree", "Testcasethree"},
+            {"testfour", "Testcasefour"}
+        };
+            
         [Button("Simple button to click one")]
         public void SimpleLog() {
             Debug.Log("Simple log");
