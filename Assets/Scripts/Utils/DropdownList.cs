1cb3bce7b49e6aa3de7600f0b1b7e02ac308c5fa
Slavik Shudenko <shudenkoslavik@gmail.com>
2021-03-01 19:59:05 +0200

    Implement dropdownlist

---
+++ Assets/Scripts/Utils/DropdownList.cs
@@ -0,0 +1,17 @@
+﻿namespace Assets.Scripts.Utils {
+    using System.Collections;
+    using System.Collections.Generic;
+
+    public class DropdownList<T> : IEnumerable<KeyValuePair<string, object>> {
+        private readonly Dictionary<string, object> data = new Dictionary<string, object>();
+
+        public void Add(string name, T element) {
+            this.data.Add(name, element);
+        }
+
+        public IEnumerator<KeyValuePair<string, object>> GetEnumerator() 
+            => this.data.GetEnumerator();
+
+        IEnumerator IEnumerable.GetEnumerator() => this.GetEnumerator();
+    }
+}
