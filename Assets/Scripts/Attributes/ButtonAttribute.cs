b64df761c5725268b8e87f9de566b8f232779323
Slavik Shudenko <shudenkoslavik@gmail.com>
2021-03-03 18:56:40 +0200

    Fix style and namings

--- Assets/Scripts/Attributes/ButtonAttribute.cs
+++ Assets/Scripts/Attributes/ButtonAttribute.cs
@@ -4,8 +4,7 @@ namespace Assets.Scripts.Attributes {
     using System;
 
     [AttributeUsage(AttributeTargets.Method)]
-    public sealed class ButtonAttribute : BaseAttribute 
-    {
+    public sealed class ButtonAttribute : BaseAttribute {
         public string Text { get; private set; }
 
         public ButtonAttribute(string text = "Button") {
