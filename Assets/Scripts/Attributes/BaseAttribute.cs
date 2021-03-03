312d168b44139db33101c266cfc353a0187094f8
Slavik Shudenko <shudenkoslavik@gmail.com>
2021-03-03 02:53:13 +0200

    Implement base and EnumFlags attributes

---
+++ Assets/Scripts/Attributes/BaseAttribute.cs
@@ -0,0 +1,7 @@
+﻿namespace Assets.Scripts.Attributes {
+    using UnityEngine;
+
+    public class BaseAttribute : PropertyAttribute, IAttribute {
+        
+    }
+}
---
+++ Assets/Scripts/Editor/PropertyDrawers/EnumFlagsDrawer.cs
@@ -0,0 +1,12 @@
+﻿namespace Assets.Scripts.Editor.PropertyDrawers {
+    using Attributes;
+    using UnityEditor;
+    using UnityEngine;
+
+    [CustomPropertyDrawer(typeof(EnumFlagsAttribute))]
+    public class EnumFlagsDrawer : BaseDrawer {
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label) {
+            base.OnGUI(position, property, label);
+        }
+    }
+}
