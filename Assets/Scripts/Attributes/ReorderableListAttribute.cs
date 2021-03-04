1d4f3d509a3746608dd3bfd38e8b9fa929cd1d17
Slavik Shudenko <shudenkoslavik@gmail.com>
2021-03-04 23:03:45 +0200

    Add ReorderableList attribute

---
+++ Assets/Scripts/Attributes/ReorderableListAttribute.cs
@@ -0,0 +1,12 @@
+﻿namespace Assets.Scripts.Attributes {
+    public class ReorderableListAttribute : BaseAttribute {
+        public string Name { get; private set; }
+
+        public ReorderableListAttribute(string name) {
+            Name = name;
+        }
+
+        public ReorderableListAttribute() {
+        }
+    }
+}
