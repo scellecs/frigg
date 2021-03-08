463a84976a7b8c22a11236231844dcea618766b1
Slavik Shudenko <shudenkoslavik@gmail.com>
2021-03-08 19:01:21 +0200

    Create ReorderableList as custom attribute

---
+++ Assets/Scripts/Attributes/Custom/ReorderableListAttribute.cs
@@ -0,0 +1,15 @@
+﻿namespace Assets.Scripts.Attributes.CustomAttribute {
+    using System;
+
+    [AttributeUsage(AttributeTargets.Field)]
+    public class ReorderableListAttribute : Attribute, IAttribute {
+        public string Name { get; private set; }
+
+        public ReorderableListAttribute(string name) {
+            this.Name = name;
+        }
+        
+        public ReorderableListAttribute() {
+        }
+    }
+}
