092e0c7d600e4cccc1047237a4f6814d735eba00
Slavik Shudenko <shudenkoslavik@gmail.com>
2021-03-09 14:41:26 +0200

    Add ShowInInspector attribute

---
+++ Assets/Scripts/Attributes/ShowInInspectorAttribute.cs
@@ -0,0 +1,8 @@
+﻿namespace Assets.Scripts.Attributes {
+    using System;
+
+    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property, AllowMultiple = true)]
+    public class ShowInInspectorAttribute : BaseAttribute {
+        
+    }
+}
