4aba558bc4a3a510befe9cb1e0ac36b6bc2d83bc
Slavik Shudenko <shudenkoslavik@gmail.com>
2021-03-16 09:39:38 +0200

    Add order attribute

---
+++ Assets/Scripts/Attributes/Meta/OrderAttribute.cs
@@ -0,0 +1,10 @@
+﻿namespace Assets.Scripts.Attributes.Meta {
+    using System;
+
+    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Method | AttributeTargets.Property, AllowMultiple = true)]
+    public class OrderAttribute : Attribute, IAttribute {
+        public OrderAttribute(int order = 0) {
+            
+        }
+    }
+}
