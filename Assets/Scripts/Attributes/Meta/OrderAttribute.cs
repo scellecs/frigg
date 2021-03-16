2b2a90b8409be7e3025edf99309e3329184b6d9f
Slavik Shudenko <shudenkoslavik@gmail.com>
2021-03-16 23:11:19 +0200

    Update tests and attribute

--- Assets/Scripts/Attributes/Meta/OrderAttribute.cs
+++ Assets/Scripts/Attributes/Meta/OrderAttribute.cs
@@ -3,8 +3,10 @@
 
     [AttributeUsage(AttributeTargets.Field | AttributeTargets.Method | AttributeTargets.Property, AllowMultiple = true)]
     public class OrderAttribute : Attribute, IAttribute {
+        public int Order { get; private set; }
+
         public OrderAttribute(int order = 0) {
-            
+            Order = order;
         }
     }
 }
---
+++ Assets/Scripts/Tests/OrderBehaviour.cs
@@ -0,0 +1,38 @@
+﻿namespace Assets.Scripts.Tests {
+    using Attributes;
+    using Attributes.Custom;
+    using Attributes.Meta;
+    using UnityEngine;
+
+    public class OrderBehaviour : MonoBehaviour {
+
+        [Readonly, Order(3)]
+        public Vector3 data = Vector3.back;
+        
+        [Order(2)]
+        public int value;
+        
+        [Button("Ordered 1"), Order(1)]
+        public void SimpleOne() {
+            Debug.Log("Simple log");
+        }
+        
+        [Button("Ordered 0"), Order]
+        public void TestTwo() {
+            Debug.Log("Simple log two");
+        }
+        
+        [Button("Ordered 0 too"), Order]
+        public void TestThree() {
+            Debug.Log("Simple log two");
+        }
+
+        [Order(5)]
+        [ShowInInspector]
+        private int TestToEdit;
+        
+        [Order(4)]
+        [ShowInInspector]
+        public int TestProperty { get; set;}
+    }
+}
