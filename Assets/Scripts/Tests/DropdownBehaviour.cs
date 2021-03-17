e4900c42803751ae21613fc723a39ba77a6a443b
Slavik Shudenko <shudenkoslavik@gmail.com>
2021-03-17 12:39:07 +0200

    Add some more tests

---
+++ Assets/Scripts/Tests/DropdownBehaviour.cs
@@ -0,0 +1,30 @@
+﻿namespace Assets.Scripts.Tests {
+    using System.Collections.Generic;
+    using Attributes;
+    using UnityEngine;
+    using Utils;
+
+    public class DropdownBehaviour : MonoBehaviour {
+        
+        [Dropdown("intValues")]
+        public int intValue;
+
+        private int[] intValues = {1, 2, 3, 4, 5};
+
+        [Dropdown("VectorValues")]
+        public Vector3 vectorValue;
+        
+        [Dropdown("StringValues")]
+        public string stringValue;
+        
+        private List<string> StringValues { get { return new List<string>() { "A", "B", "C", "D", "E" }; } }
+
+        private DropdownList<Vector3> VectorValues() => 
+            new DropdownList<Vector3>() {
+                {"test", Vector3.back},
+                {"testtwo", Vector3.up},
+                {"testthree", Vector3.forward},
+                {"testfour", Vector3.left}
+            };
+    }
+}
