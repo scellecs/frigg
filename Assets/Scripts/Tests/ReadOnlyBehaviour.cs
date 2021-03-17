9d6c6384670b6085d0d5a71c9a6c572a44709a28
Slavik Shudenko <shudenkoslavik@gmail.com>
2021-03-17 11:19:39 +0200

    Add readonly tests

---
+++ Assets/Scripts/Tests/ReadOnlyBehaviour.cs
@@ -0,0 +1,16 @@
+﻿namespace Assets.Scripts.Tests {
+    using Attributes;
+    using UnityEngine;
+
+    public class ReadOnlyBehaviour : MonoBehaviour {
+        
+        [Readonly]
+        public Vector3 data = Vector3.back;
+
+        [Readonly]
+        public int testInt = 123;
+        
+        [Readonly]
+        public string testString = "test string data";
+    }
+}
