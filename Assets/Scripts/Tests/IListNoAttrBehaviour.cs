b299d3da5759b53e6d387587abce5904704d3652
Slavik Shudenko <shudenkoslavik@gmail.com>
2021-03-19 19:24:21 +0200

    Add tests to IList without attributes

---
+++ Assets/Scripts/Tests/IListNoAttrBehaviour.cs
@@ -0,0 +1,19 @@
+﻿namespace Assets.Scripts.Tests {
+    using System.Collections.Generic;
+    using UnityEngine;
+
+    public class IListNoAttrBehaviour : MonoBehaviour {
+        
+        public int[] intArray;
+        
+        public List<int> intlist;
+        
+        public float[]   floatArray;
+        
+        public List<float> floatlist;
+        
+        public Vector3[] vectorArray;
+        
+        public List<Vector3> vectorlist;
+    }
+}
