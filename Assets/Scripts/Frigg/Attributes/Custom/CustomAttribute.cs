1c0ecfb2029baed45aab064aff828161950fb703
Slavik Shudenko <shudenkoslavik@gmail.com>
2021-03-23 19:38:07 +0200

    To package

---
+++ Assets/Scripts/Frigg/Attributes/Custom/CustomAttribute.cs
@@ -0,0 +1,10 @@
+﻿namespace Assets.Scripts.Attributes.Custom {
+    using System;
+    using System.Collections.Generic;
+    using Frigg;
+    using UnityEditor;
+
+    public class CustomAttribute : Attribute, IAttribute {
+
+    }
+}
