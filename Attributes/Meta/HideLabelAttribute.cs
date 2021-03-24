3af1725c0bb250d687601c5a31f2d4ba09d4a931
Slavik Shudenko <shudenkoslavik@gmail.com>
2021-03-24 21:25:42 +0200

    Add HideLabel attribute

---
+++ Attributes/Meta/HideLabelAttribute.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Frigg;
+
+namespace Assets.Scripts.Attributes.Meta
+{
+    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Method | AttributeTargets.Property, AllowMultiple = true)]
+    public class HideLabelAttribute : Attribute, IAttribute
+    {
+        
+    }
+}
