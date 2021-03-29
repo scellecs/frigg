7595b7cbce7ac374769df025d008bba725e7669a
Slavik Shudenko <shudenkoslavik@gmail.com>
2021-03-29 18:58:37 +0300

    Some tests

---
+++ Editor/Tests/InlinePropertyBehaviour.cs
@@ -0,0 +1,28 @@
+﻿namespace Assets.Scripts.Tests {
+    using System;
+    using Attributes.Custom;
+    using UnityEngine;
+
+    public class InlinePropertyBehaviour : MonoBehaviour {
+
+        [InlineProperty(LabelWitdh = 10)]
+        public Player    player;
+        
+        public PlayerTwo playerTwo;
+
+        [Serializable]
+        public struct Player {
+            public int health;
+            public int armor;
+            public int damage;
+        }
+        
+        [Serializable]
+        [InlineProperty(LabelWitdh = 10)]
+        public class PlayerTwo {
+            public int healthTwo;
+            public int armorTwo;
+            public int damageTwo;
+        }
+    }
+}
