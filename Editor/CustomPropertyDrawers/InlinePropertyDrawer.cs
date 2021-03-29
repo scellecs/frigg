a7bdb4f7ec63aa410a2f470b7178ab874e3de0e0
Slavik Shudenko <shudenkoslavik@gmail.com>
2021-03-29 18:57:32 +0300

    Implement InlinePropertyDrawer

---
+++ Editor/CustomPropertyDrawers/InlinePropertyDrawer.cs
@@ -0,0 +1,30 @@
+﻿namespace Assets.Scripts.Editor.PropertyDrawers {
+    using Attributes.Custom;
+    using UnityEditor;
+    using UnityEngine;
+    using Utils;
+    using CustomPropertyDrawer = CustomPropertyDrawers.CustomPropertyDrawer;
+
+    public class InlinePropertyDrawer : CustomPropertyDrawer {
+        public static readonly InlinePropertyDrawer instance = new InlinePropertyDrawer();
+
+        public int labelWidth;
+        
+        protected override void CreateAndDraw(Rect rect, SerializedProperty property, GUIContent label) {
+            EditorGUILayout.BeginHorizontal();
+
+            EditorGUIUtility.labelWidth = this.labelWidth;
+            EditorGUILayout.LabelField(label);
+
+            var copy       = property.Copy(); //we need to work with a property copy
+            var enumerator = copy.GetEnumerator();
+            
+            while (enumerator.MoveNext()) {
+                var prop = (SerializedProperty) enumerator.Current;
+                EditorGUILayout.PropertyField(prop);
+            }
+
+            EditorGUILayout.EndHorizontal();
+        }
+    }
+}
