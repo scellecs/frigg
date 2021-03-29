62ded40b79590217b94b779560c672ed0aae0861
Slavik Shudenko <shudenkoslavik@gmail.com>
2021-03-29 18:57:48 +0300

    Add InlinePropertyAttribute

---
+++ Attributes/Custom/InlinePropertyAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Assets.Scripts.Attributes.Custom {
+    using Custom;
+    using Frigg;
+
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct | 
+                    AttributeTargets.Field | AttributeTargets.Property, Inherited = false)]
+    public class InlinePropertyAttribute : CustomAttribute {
+
+        public int LabelWitdh { get; set; }
+
+        public InlinePropertyAttribute() {
+            LabelWitdh = 0;
+        }
+    }
+}
