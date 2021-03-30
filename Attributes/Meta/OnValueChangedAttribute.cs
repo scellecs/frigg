aee9c93ed5dca0b18aec7ad0076b69b9e049dd56
Slavik Shudenko <shudenkoslavik@gmail.com>
2021-03-30 20:44:21 +0300

    Add OnValueChanged attribute

---
+++ Attributes/Meta/OnValueChangedAttribute.cs
@@ -0,0 +1,13 @@
+﻿namespace Assets.Scripts.Attributes.Meta {
+    using System;
+    using Frigg;
+
+    public class OnValueChangedAttribute : Attribute, IAttribute {
+
+        public string callbackMethod { get; private set; }
+
+        public OnValueChangedAttribute(string callback) {
+            this.callbackMethod = callback;
+        }
+    }
+}
