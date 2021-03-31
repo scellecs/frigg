3e9cb49a6ee252de7c2e93e7410c3f3bdde9b556
Slavik Shudenko <shudenkoslavik@gmail.com>
2021-03-31 12:35:35 +0300

    Add BaseDecoratorAttribute

---
+++ Attributes/Decorators/BaseDecoratorAttribute.cs
@@ -0,0 +1,9 @@
+﻿namespace Packages.Frigg.Attributes {
+    using global::Frigg;
+    using UnityEditor.Graphs;
+    using UnityEngine;
+
+    public class BaseDecoratorAttribute : PropertyAttribute, IAttribute {
+        
+    }
+}
