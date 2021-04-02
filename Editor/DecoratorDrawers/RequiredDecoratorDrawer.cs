0354f3bc86093384da28652f4d2a7f81bf321bb1
Slavik Shudenko <shudenkoslavik@gmail.com>
2021-04-02 20:47:47 +0300

    RequiredAttribute drawer

---
+++ Editor/DecoratorDrawers/RequiredDecoratorDrawer.cs
@@ -0,0 +1,42 @@
+﻿namespace Assets.Scripts.Editor.DecoratorDrawers {
+    using Packages.Frigg.Attributes;
+    using UnityEditor;
+    using UnityEngine;
+
+    public class RequiredDecoratorDrawer : BaseDecoratorDrawer {
+        protected override float GetHeight(Rect rect) {
+            return EditorGUIUtility.singleLineHeight + SPACE_AMOUNT;
+        }
+
+        protected override void DrawDecorator(Rect rect, object target) {
+            var attr = (RequiredAttribute) this.attribute;
+
+            var serializedProperty = (SerializedProperty) target;
+
+            if (this.property.propertyType == SerializedPropertyType.ObjectReference) {
+                if (this.property.objectReferenceValue == null) {
+                    if (string.IsNullOrEmpty(attr.Text)) {
+                        attr.Text = $"{serializedProperty.displayName} is required!";
+                    }
+                }
+            }
+            
+            else { 
+                attr.Text = $"{nameof(RequiredAttribute)} can be used only on reference types";
+            }
+            
+            var content = EditorGUIUtility.TrTextContentWithIcon(attr.Text, MessageType.Error);
+            var style = new GUIStyle(EditorStyles.helpBox) {
+                fontSize  = 14,
+                alignment = TextAnchor.MiddleLeft
+            };
+            
+            var height = style.CalcHeight(content, rect.width);
+            if (rect.height < height) {
+                rect.height += height - rect.height;
+            }
+            
+            GUI.Label(rect, content, style);
+        }
+    }
+}
