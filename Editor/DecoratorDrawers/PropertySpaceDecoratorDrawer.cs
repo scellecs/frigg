c8154e795571aac3fb4bdf0a57f7b8fd86d87125
Slavik Shudenko <shudenkoslavik@gmail.com>
2021-04-02 22:21:39 +0300

    Add PropertySpace drawer

---
+++ Editor/DecoratorDrawers/PropertySpaceDecoratorDrawer.cs
@@ -0,0 +1,19 @@
+﻿namespace Assets.Scripts.Editor.DecoratorDrawers {
+    using Packages.Frigg.Attributes;
+    using UnityEditor;
+    using UnityEngine;
+
+    public class PropertySpaceDecoratorDrawer : BaseDecoratorDrawer {
+        protected override float GetHeight(Rect rect) {
+            var attr = (PropertySpaceAttribute) this.attribute;
+            
+            return attr.SpaceBefore;
+        }
+
+        protected override void DrawDecorator(Rect rect, object target) {
+            var attr = (PropertySpaceAttribute) this.attribute;
+            
+            EditorGUILayout.Space(this.GetHeight(rect));
+        }
+    }
+}
