ec63660db1e96f49a940b8851535218f9bb1b33f
Slavik Shudenko <shudenkoslavik@gmail.com>
2021-04-02 22:21:20 +0300

    Add PropertySpace attribute

---
+++ Attributes/Decorators/PropertySpaceAttribute.cs
@@ -0,0 +1,19 @@
+﻿namespace Packages.Frigg.Attributes {
+    using System;
+
+    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Method | AttributeTargets.Property, AllowMultiple = true)]
+    public class PropertySpaceAttribute : BaseDecoratorAttribute {
+        private int spaceBefore = 0;
+        private int spaceAfter  = 0;
+
+        public int SpaceBefore {
+            get => this.spaceBefore;
+            set => this.spaceBefore = value;
+        }
+
+        public int SpaceAfter {
+            get => this.spaceAfter;
+            set => this.spaceAfter = value;
+        }
+    }
+}
