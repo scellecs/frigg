3a6ba52987683dad648d821c983928b78620befa
Slavik Shudenko <shudenkoslavik@gmail.com>
2021-04-02 20:49:05 +0300

    Some tests

---
+++ Editor/Tests/RequiredBehaviour.cs
@@ -0,0 +1,12 @@
+﻿namespace Assets.Scripts.Tests {
+    using Packages.Frigg.Attributes;
+    using UnityEngine;
+
+    public class RequiredBehaviour : MonoBehaviour {
+        [Required]
+        public GameObject testObject;
+        
+        [Required("I am a custom dude")]
+        public Transform testTransform;
+    }
+}
