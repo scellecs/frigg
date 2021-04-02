40466d51d17cd3c308c1a54f0d705c26b3d735cf
Slavik Shudenko <shudenkoslavik@gmail.com>
2021-04-02 17:30:27 +0300

    Tests

---
+++ Editor/Tests/InfoBoxBehaviour.cs
@@ -0,0 +1,16 @@
+﻿namespace Assets.Scripts.Tests {
+    using Packages.Frigg.Attributes;
+    using UnityEngine;
+
+    public class InfoBoxBehaviour : MonoBehaviour {
+        [Title("InfoBox attribute")]
+        [InfoBox("Test box type Info")]
+        public int testInteger;
+        
+        [InfoBox("Test box type Warning & fixed height", InfoMessageType = InfoMessageType.Warning, Height = 15)]
+        public int testIntegerTwo;
+        
+        [InfoBox("Test box type Error & custom font size", InfoMessageType = InfoMessageType.Error, FontSize = 16)]
+        public int testIntegerThree;
+    }
+}
