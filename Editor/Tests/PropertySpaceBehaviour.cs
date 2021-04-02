3fa93a546757ba6773523c2dffc3d8b60572072d
Slavik Shudenko <shudenkoslavik@gmail.com>
2021-04-02 22:21:29 +0300

    Add PropertySpace tests

---
+++ Editor/Tests/PropertySpaceBehaviour.cs
@@ -0,0 +1,21 @@
+﻿namespace Assets.Scripts.Tests {
+    using Attributes;
+    using Attributes.Meta;
+    using Packages.Frigg.Attributes;
+    using UnityEngine;
+
+    public class PropertySpaceBehaviour : MonoBehaviour {
+        
+        [ShowInInspector]
+        private int testInteg;
+
+        [PropertySpace(SpaceBefore = 20), Order(1)]
+        public float testFloat;
+
+        [PropertySpace(SpaceBefore = 10)]
+        [Button, Order(1)]
+        public void ClickMe() {
+            
+        }
+    }
+}
