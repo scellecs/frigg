dc82c1b18cb1d7d1d7ad0a32258a8462c06f255f
Slavik Shudenko <shudenkoslavik@gmail.com>
2021-04-06 16:39:45 +0300

    Some tests

---
+++ Editor/Tests/PropertyTooltipBehaviour.cs
@@ -0,0 +1,24 @@
+﻿namespace Assets.Scripts.Tests {
+    using Attributes;
+    using Attributes.Meta;
+    using UnityEngine;
+
+    public class PropertyTooltipBehaviour : MonoBehaviour {
+        [PropertyTooltip("It's private non serialized field")]
+        [ShowInInspector]
+        private int test;
+
+        [PropertyTooltip("It's public field")]
+        public int publicTest;
+
+        [PropertyTooltip("It's property")]
+        [ShowInInspector]
+        public int PropertyTest { get; set; }
+
+        [PropertyTooltip("It's button")]
+        [Button]
+        public void ButtonTest() {
+            
+        }
+    }
+}
