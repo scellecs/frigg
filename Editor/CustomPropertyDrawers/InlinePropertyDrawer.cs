1c4e0aa8809f1592fc73ca7207a938a6302bf9a7
Slavik Shudenko <shudenkoslavik@gmail.com>
2021-04-28 23:17:55 +0300

    Implement inlinePropertyDrawer

--- Editor/CustomPropertyDrawers/InlinePropertyDrawer.cs
+++ Editor/CustomPropertyDrawers/InlinePropertyDrawer.cs
@@ -1,7 +1,10 @@
 ﻿namespace Frigg.Editor {
+    using System.Collections;
+    using System.Collections.Generic;
     using System.Reflection;
     using UnityEditor;
     using UnityEngine;
+    using Utils;
 
     public class InlinePropertyDrawer : CustomPropertyDrawer {
         public static readonly InlinePropertyDrawer instance = new InlinePropertyDrawer();
@@ -28,8 +31,49 @@
             EditorGUIUtility.labelWidth = cachedWidth;
         }
 
-        protected override void CreateAndDraw(object target, Rect rect, MemberInfo memberInfo, GUIContent label) {
-            throw new System.NotImplementedException();
+        //TODO: Refactor
+        protected override object CreateAndDraw(Rect rect, MemberInfo memberInfo, object target, GUIContent content) {
+            var objType = target.GetType();
+            var members = new List<(MemberInfo, object)>();
+            target.TryGetMembers(members);
+
+            foreach (var (info, member) in members) {
+                var writable = CoreUtilities.IsWritable(info);
+
+                var field    = objType.GetField(info.Name);
+                if (field != null) {
+                    if (typeof(IList).IsAssignableFrom(field.FieldType) && field.FieldType != typeof(string)) {
+                        var drawer = (ReorderableListDrawer) CustomAttributeExtensions.GetCustomDrawer(typeof(ReorderableListAttribute));
+                        var value  = drawer.OnGUI(member, Rect.zero, field, content);
+                        field.SetValue(target, value);
+                        continue;
+                    }
+                    
+                    var objValue = field.GetValue(target);
+                    if (rect == Rect.zero)
+                        field.SetValue(target, GuiUtilities.LayoutField(field.FieldType, objValue, CoreUtilities.GetGUIContent(info), writable));
+                    else {
+                        field.SetValue(target, GuiUtilities.Field(field.FieldType, objValue, rect, CoreUtilities.GetGUIContent(info), writable));
+                        rect.y += EditorGUIUtility.singleLineHeight + GuiUtilities.SPACE;
+                    }
+                }
+                        
+                var property = objType.GetProperty(info.Name);
+                if (property != null) {
+                    if (typeof(IList).IsAssignableFrom(property.PropertyType) && property.PropertyType != typeof(string)) {
+                        var drawer = (ReorderableListDrawer) CustomAttributeExtensions.GetCustomDrawer(typeof(ReorderableListAttribute));
+                        property.SetValue(target, drawer.OnGUI(property, Rect.zero, property, content));
+                        continue;
+                    }
+                    
+                    var objValue = property.GetValue(target);
+                    if (objValue == null)
+                        return null;
+                    property.SetValue(target, GuiUtilities.LayoutField(property.PropertyType, objValue, CoreUtilities.GetGUIContent(info), writable));
+                }
+            }
+
+            return target;
         }
     }
 }
