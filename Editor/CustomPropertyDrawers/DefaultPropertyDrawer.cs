fc33da2bb73609c2eeab87eccc2953048cb1e111
Slavik Shudenko <shudenkoslavik@gmail.com>
2021-05-04 19:58:15 +0300

    Implement DefaultPropertyDrawer & FoldoutPropertyDrawer

---
+++ Editor/CustomPropertyDrawers/DefaultPropertyDrawer.cs
@@ -0,0 +1,15 @@
+﻿namespace Frigg.Editor {
+    using System.Reflection;
+    using UnityEditor;
+    using UnityEngine;
+
+    public class DefaultPropertyDrawer : CustomPropertyDrawer {
+        protected override void CreateAndDraw(SerializedProperty property, GUIContent label) {
+            throw new System.NotImplementedException();
+        }
+
+        protected override object CreateAndDraw(Rect rect, MemberInfo member, object target, GUIContent label) {
+            return null;
+        }
+    }
+}
---
+++ Editor/CustomPropertyDrawers/FoldoutPropertyDrawer.cs
@@ -0,0 +1,28 @@
+﻿namespace Frigg.Editor {
+    using System.Collections.Generic;
+    using System.Reflection;
+    using UnityEditor;
+    using UnityEngine;
+
+    public class FoldoutPropertyDrawer : CustomPropertyDrawer {
+        public static readonly FoldoutPropertyDrawer     instance      = new FoldoutPropertyDrawer();
+        public                 Dictionary<string, bool> foldoutValues = new Dictionary<string, bool>();
+        
+        protected override void CreateAndDraw(SerializedProperty property, GUIContent content) {
+            //throw new System.NotImplementedException();
+        }
+
+        protected override object CreateAndDraw(Rect rect, MemberInfo member, object target, GUIContent content) {
+            if (!instance.foldoutValues.ContainsKey(member.Name)) {
+                instance.foldoutValues.Add(member.Name, false);
+            }
+            
+            instance.foldoutValues[member.Name] = EditorGUILayout.BeginFoldoutHeaderGroup
+                (instance.foldoutValues[member.Name], content);
+            
+            EditorGUILayout.EndFoldoutHeaderGroup();
+
+            return target;
+        }
+    }
+}
