8222f8ecfe9ecc7dd3a763edd5f321ab09959bfd
Slavik Shudenko <shudenkoslavik@gmail.com>
2021-05-13 18:07:11 +0300

    UniversalProperty Implementation

---
+++ Editor/UniversalProperty.cs
@@ -0,0 +1,14 @@
+﻿namespace Frigg.Editor {
+    using System.Collections.Generic;
+    using UnityEngine;
+
+    public class UniversalProperty {
+        public GUIContent Label { get; set; }
+
+        public UniversalProperty ParentProperty { get; set; }
+
+        public PropertyTree PropertyTree { get; set; }
+
+        private IEnumerable<UniversalProperty> ChildrenProperties { get; set; }
+    }
+}
