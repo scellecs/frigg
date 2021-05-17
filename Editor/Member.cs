c767733117a4c29a83aeb02d0dcfc93db7929884
Slavik Shudenko <shudenkoslavik@gmail.com>
2021-05-17 17:44:25 +0300

    Discard member struct

--- Editor/Member.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Frigg.Editor {
-    using System.Reflection;
-
-    public class Member {
-        public MemberInfo memberInfo;
-        public object     target;
-        public bool       isArray;
-    }
-}
