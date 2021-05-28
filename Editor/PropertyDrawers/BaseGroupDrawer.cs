ab312d73433b8336cc9a7ad4da7223b62438be4d
Slavik Shudenko <shudenkoslavik@gmail.com>
2021-05-28 17:23:19 +0300

    Base implementation of Group Drawer

---
+++ Editor/PropertyDrawers/BaseGroupDrawer.cs
@@ -0,0 +1,19 @@
+﻿namespace Frigg.Editor {
+    using System.Collections.Generic;
+    using Groups;
+    using Packages.Frigg.Editor.Utils;
+    using UnityEngine;
+
+    public abstract class BaseGroupDrawer : FriggPropertyDrawer {
+        private BaseGroupAttribute attribute;
+        
+        private Rect baseRect = Rect.zero;
+        private Rect currentRect;
+
+        public static Dictionary<GroupInfo, BaseGroupDrawer> activeGroups = new Dictionary<GroupInfo, BaseGroupDrawer>();
+        
+        public BaseGroupDrawer(FriggProperty prop) : base(prop) {
+            this.attribute = (BaseGroupAttribute) this.linkedAttribute;
+        }
+    }
+}
