d0053d03430e75e09f7f15eb0e32d2d44e735f8c
Slavik Shudenko <shudenkoslavik@gmail.com>
2021-05-28 22:11:48 +0300

    Group handler for Inline Drawer (need to handle this better)

--- Editor/PropertyDrawers/InlinePropertyDrawer.cs
+++ Editor/PropertyDrawers/InlinePropertyDrawer.cs
@@ -1,5 +1,7 @@
 ﻿namespace Frigg.Editor {
     using System;
+    using Groups;
+    using Packages.Frigg.Editor.Utils;
     using UnityEditor;
     using UnityEngine;
 
@@ -16,11 +18,28 @@
         public override void Draw(Rect rect) {
             this.property.IsExpanded = true;
             foreach (var p in this.property.ChildrenProperties.RecurseChildren()) {
-                p.Draw(rect);
-                var h = FriggProperty.GetPropertyHeight(p);
-                rect.y      += h + GuiUtilities.SPACE / 2f;
-                rect.height =  h + GuiUtilities.SPACE / 2f;
+                var attr    = p.TryGetFixedAttribute<BaseGroupAttribute>();
+                if (attr != null) {
+                    rect.width = attr.ElementWidth;
+                    var group   = new GroupInfo(attr.GroupName, p.ParentProperty.Path);
+                    if (GroupsHandler.IsDefault(group)) {
+                        GroupsHandler.Init(group, rect);
+                    }
+                    
+                    var newRect = GroupsHandler.GetLastRect(group);
+                    rect = newRect;
+                    p.Draw(rect);
+                    GroupsHandler.UpdateGroupInfo(group, new Rect(rect.x + attr.ElementWidth, rect.y, GroupsHandler.GetAvailableWidth(group) - 105, rect.height));
+                }
+
+                else {
+                    var h = FriggProperty.GetPropertyHeight(p);
+                    p.Draw(rect);
+                    rect.y     += h + GuiUtilities.SPACE / 2f;
+                }
             }
+            
+            GroupsHandler.ResetGroups();
         }
 
         public override float GetHeight() => FriggProperty.GetPropertyHeight(this.property);
