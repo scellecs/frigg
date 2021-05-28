140108e5c898be03f3c757b014450e26082f025b
Slavik Shudenko <shudenkoslavik@gmail.com>
2021-05-28 17:19:40 +0300

    InfoBox drawer

--- Editor/DecoratorDrawers/InfoBoxDecoratorDrawer.cs
+++ Editor/DecoratorDrawers/InfoBoxDecoratorDrawer.cs
@@ -3,8 +3,66 @@
     using UnityEditor;
     using UnityEngine;
 
-    public class InfoBoxDecoratorDrawer : BaseDecoratorDrawer {
-        public override bool IsVisible(SerializedProperty prop) => true;
+    public class InfoBoxDecoratorDrawer : FriggDecoratorDrawer {
+        public override void DrawLayout() {
+            var attr = (InfoBoxAttribute) this.linkedAttribute;
+
+            MessageType messageType;
+            switch (attr.InfoMessageType) {
+                case InfoMessageType.None:
+                    messageType = MessageType.None;
+                    break;
+                case InfoMessageType.Info:
+                    messageType = MessageType.Info;
+                    break;
+                case InfoMessageType.Warning:
+                    messageType = MessageType.Warning;
+                    break;
+                case InfoMessageType.Error:
+                    messageType = MessageType.Error;
+                    break;
+                default:
+                    messageType = MessageType.Info;
+                    break;
+            }
+
+            EditorGUILayout.HelpBox(attr.Text, messageType);
+            this.property.CallNextDrawer();
+        }
+
+        public override void Draw(Rect rect) {
+            var       temp   = rect;
+            const int height = BaseDecoratorAttribute.DEFAULT_HEIGHT;
+            temp.height = height;
+            var infoBox = (InfoBoxAttribute) this.linkedAttribute;
+            
+            EditorGUI.HelpBox(temp, infoBox.Text, this.GetMessageType(infoBox.InfoMessageType));
+            rect.y += height;
+            this.property.CallNextDrawer(rect);
+        }
+
+        private MessageType GetMessageType(InfoMessageType type) {
+            switch (type) {
+                case InfoMessageType.None:
+                    return MessageType.None;
+                
+                case InfoMessageType.Info:
+                    return MessageType.Info;
+                
+                case InfoMessageType.Warning:
+                    return MessageType.Warning;
+                
+                case InfoMessageType.Error:
+                    return MessageType.Error;
+                
+                default:
+                    return MessageType.Info;
+            }
+        }
+
+        public override bool  IsVisible   => true;
+        public override float GetHeight() => ((InfoBoxAttribute)this.linkedAttribute).Height;
+        /*public override bool IsVisible(SerializedProperty prop) => true;
 
         protected override float GetHeight(Rect rect) {
             var attr = (InfoBoxAttribute) this.attribute;
@@ -58,6 +116,6 @@
             
             if(!isArray)
                 EditorGUILayout.Space(this.GetHeight(EditorGUILayout.GetControlRect()));
-        }
+        }*/
     }   
 }
