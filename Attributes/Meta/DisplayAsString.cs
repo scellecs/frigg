6c2d06933bff5139a3c6c236eae3a452849eec7c
Slavik Shudenko <shudenkoslavik@gmail.com>
2021-05-28 17:21:48 +0300

    DisplayAsString attribute

--- Attributes/Meta/DisplayAsString.cs
+++ Attributes/Meta/DisplayAsString.cs
@@ -3,6 +3,10 @@
 
     [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property)]
     public class DisplayAsString : BaseAttribute {
-        
+        public bool Value { get; set; }
+
+        public DisplayAsString(bool value) {
+            this.Value = value;
+        }
     }
 }
