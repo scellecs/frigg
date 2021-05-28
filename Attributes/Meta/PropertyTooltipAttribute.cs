5e57ac8152e7a77d3a03b8cb3d16be50eec3207a
Slavik Shudenko <shudenkoslavik@gmail.com>
2021-05-28 17:17:52 +0300

    Tooltip drawer

--- Attributes/Meta/PropertyTooltipAttribute.cs
+++ Attributes/Meta/PropertyTooltipAttribute.cs
@@ -4,10 +4,12 @@
     [AttributeUsage(AttributeTargets.Field | AttributeTargets.Method | AttributeTargets.Property | AttributeTargets.Class | AttributeTargets.Struct, 
         AllowMultiple = true)]
     public class PropertyTooltipAttribute : Attribute, IAttribute {
-        public string Text { get; private set; }
+        public string Text      { get; set; }
+        public bool   IsDynamic { get; set; }
 
-        public PropertyTooltipAttribute(string text) {
-            this.Text = text;
+        public PropertyTooltipAttribute(string text, bool isDynamic) {
+            this.Text      = text;
+            this.IsDynamic = isDynamic;
         }
     }
 }
