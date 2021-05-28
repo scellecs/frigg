286eb65e2494e3cb258b390dc1c7f563785ac88c
Slavik Shudenko <shudenkoslavik@gmail.com>
2021-05-28 17:10:23 +0300

    Rect drawer

---
+++ Editor/PropertyDrawers/BuiltIn/RectDrawer.cs
@@ -0,0 +1,26 @@
+﻿namespace Frigg.Editor.BuiltIn {
+    using UnityEditor;
+    using UnityEngine;
+
+    public class RectDrawer : BuiltInDrawer {
+        public RectDrawer(FriggProperty prop) : base(prop) {
+        }
+
+        public override void DrawLayout() {
+            var value  = this.GetTargetValue<Rect>();
+            var result = EditorGUILayout.RectField(this.property.Label, value);
+            this.UpdateAndCallNext(result);
+        }
+
+        public override void Draw(Rect rect) {
+            var value  = this.GetTargetValue<Rect>();
+            var result = EditorGUI.RectField(rect, this.property.Label, value);
+            this.UpdateAndCallNext(result, rect);
+        }
+
+        public override float GetHeight() => EditorGUIUtility.singleLineHeight;
+
+        public override bool IsVisible => true;
+        
+    }
+}
