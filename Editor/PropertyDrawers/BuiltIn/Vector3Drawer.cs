f1c56b834ffd5c6efb2b2658300fa7c6928c410c
Slavik Shudenko <shudenkoslavik@gmail.com>
2021-05-28 17:09:41 +0300

    Vector3D drawer

---
+++ Editor/PropertyDrawers/BuiltIn/Vector3Drawer.cs
@@ -0,0 +1,27 @@
+﻿namespace Frigg.Editor.BuiltIn {
+    using UnityEditor;
+    using UnityEngine;
+
+    public class Vector3Drawer : BuiltInDrawer {
+        public Vector3Drawer(FriggProperty prop) : base(prop) {
+        }
+
+        public override void DrawLayout() {
+            var value  = this.GetTargetValue<Vector3>();
+            var result = EditorGUILayout.Vector3Field(this.property.Label, value);
+            this.UpdateAndCallNext(result);
+        }
+
+        public override void Draw(Rect rect) {
+            var value  = this.GetTargetValue<Vector3>();
+            var result = EditorGUI.Vector3Field(rect, this.property.Label, value);
+            rect.y += EditorGUIUtility.singleLineHeight;
+            this.UpdateAndCallNext(result, rect);
+        }
+
+        public override float GetHeight() => EditorGUIUtility.singleLineHeight;
+
+        public override bool IsVisible => true;
+        
+    }
+}
