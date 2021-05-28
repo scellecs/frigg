b389138b3cfb05cdcd5292fd28aad85aec340f6a
Slavik Shudenko <shudenkoslavik@gmail.com>
2021-05-28 17:14:28 +0300

    Bool drawer

---
+++ Editor/PropertyDrawers/BuiltIn/BoolDrawer.cs
@@ -0,0 +1,26 @@
+﻿namespace Frigg.Editor.BuiltIn {
+    using UnityEditor;
+    using UnityEngine;
+
+    public class BoolDrawer : BuiltInDrawer{
+        public BoolDrawer(FriggProperty prop) : base(prop) {
+        }
+
+        public override void DrawLayout() {
+            var value = this.GetTargetValue<bool>();
+            var result = EditorGUILayout.Toggle(this.property.Label, value);
+            this.UpdateAndCallNext(result);
+        }
+
+        public override void Draw(Rect rect) {
+            var value  = this.GetTargetValue<bool>();
+            var result = EditorGUI.Toggle(rect, this.property.Label, value);
+            rect.y += EditorGUIUtility.singleLineHeight;
+            this.UpdateAndCallNext(result, rect);
+        }
+
+        public override float GetHeight() => EditorGUIUtility.singleLineHeight;
+
+        public override bool IsVisible => true;
+    }
+}
