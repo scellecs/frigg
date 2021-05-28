f8750ebc3112624ba4c9cb04dc70c44e4ab4d065
Slavik Shudenko <shudenkoslavik@gmail.com>
2021-05-28 17:16:46 +0300

    Foldout Property drawer

--- Editor/PropertyDrawers/FoldoutPropertyDrawer.cs
+++ Editor/PropertyDrawers/FoldoutPropertyDrawer.cs
@@ -5,9 +5,53 @@
     using UnityEngine;
 
     public class FoldoutPropertyDrawer : FriggPropertyDrawer {
-        public static FoldoutPropertyDrawer instance;
-        public override void DrawLayout() {
-            throw new System.NotImplementedException();
+        public FoldoutPropertyDrawer(FriggProperty prop) : base(prop) {
         }
+        
+        public override void DrawLayout() {
+            var style = EditorStyles.foldoutHeader;
+            this.property.IsExpanded = GUILayout.Toggle(this.property.IsExpanded, this.property.Label, style);
+            
+            if (!this.property.IsExpanded) {
+                return;
+            }
+
+            foreach (var p in this.property.ChildrenProperties.RecurseChildren()) {
+                p.Draw();
+            }
+        }
+
+        public override void Draw(Rect rect) {
+            
+            //To editor tools
+            var style = EditorStyles.foldoutHeader;
+
+            var toggleRect = rect;
+            var indent = EditorGUI.indentLevel * GuiUtilities.SPACE;
+            toggleRect.width         -= indent;
+            toggleRect.x             += indent;
+            this.property.IsExpanded =  GUI.Toggle(toggleRect, this.property.IsExpanded, this.property.Label, style);
+            
+            if (!this.property.IsExpanded) {
+                return;
+            }
+
+            rect.y      += EditorGUIUtility.singleLineHeight;
+
+            EditorGUI.indentLevel++;
+            foreach (var p in this.property.ChildrenProperties.RecurseChildren()) {
+                p.Draw(rect);
+                var h = FriggProperty.GetPropertyHeight(p);
+                rect.y      += h + GuiUtilities.SPACE / 2f;
+                rect.height =  EditorGUIUtility.singleLineHeight;
+            }
+            EditorGUI.indentLevel--;
+        }
+
+        public override float GetHeight() {
+            return FriggProperty.GetPropertyHeight(this.property);
+        }
+
+        public override bool IsVisible => true;
     }
 }
