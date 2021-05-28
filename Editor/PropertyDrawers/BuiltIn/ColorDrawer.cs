a50401559885ac603f5653fd9c316bc44033149b
Slavik Shudenko <shudenkoslavik@gmail.com>
2021-05-28 17:09:30 +0300

    Vector2D drawer

---
+++ Editor/PropertyDrawers/BuiltIn/ColorDrawer.cs
@@ -0,0 +1,25 @@
+﻿namespace Frigg.Editor.BuiltIn {
+    using UnityEditor;
+    using UnityEngine;
+
+    public class ColorDrawer : BuiltInDrawer {
+        public ColorDrawer(FriggProperty prop) : base(prop) {
+        }
+
+        public override void DrawLayout() {
+            var value  = this.GetTargetValue<Color>();
+            var result = EditorGUILayout.ColorField(this.property.Label, value);
+            this.UpdateAndCallNext(result);
+        }
+
+        public override void Draw(Rect rect) {
+            var value  = this.GetTargetValue<Color>();
+            var result = EditorGUI.ColorField(rect, this.property.Label, value);
+            this.UpdateAndCallNext(result, rect);
+        }
+
+        public override float GetHeight() => EditorGUIUtility.singleLineHeight;
+
+        public override bool IsVisible => true;
+    }
+}
---
+++ Editor/PropertyDrawers/BuiltIn/Vector2Drawer.cs
@@ -0,0 +1,27 @@
+﻿namespace Frigg.Editor.BuiltIn {
+    using UnityEditor;
+    using UnityEngine;
+
+    public class Vector2Drawer : BuiltInDrawer {
+        public Vector2Drawer(FriggProperty prop) : base(prop) {
+        }
+
+        public override void DrawLayout() {
+            var value  = this.GetTargetValue<Vector2>();
+            var result = EditorGUILayout.Vector2Field(this.property.Label, value);
+            this.UpdateAndCallNext(result);
+        }
+
+        public override void Draw(Rect rect) {
+            var value  = this.GetTargetValue<Vector2>();
+            var result = EditorGUI.Vector2Field(rect, this.property.Label, value);
+            rect.y += EditorGUIUtility.singleLineHeight;
+            this.UpdateAndCallNext(result, rect);
+        }
+
+        public override float GetHeight() => EditorGUIUtility.singleLineHeight;
+
+        public override bool IsVisible => true;
+        
+    }
+}
