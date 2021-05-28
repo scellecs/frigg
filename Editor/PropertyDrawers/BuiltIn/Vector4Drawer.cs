390cd966afcf7dc961b68df490de4edd93a5491b
Slavik Shudenko <shudenkoslavik@gmail.com>
2021-05-28 17:09:49 +0300

    Vector4D drawer

---
+++ Editor/PropertyDrawers/BuiltIn/Vector4Drawer.cs
@@ -0,0 +1,29 @@
+﻿namespace Frigg.Editor.BuiltIn {
+    using System.Numerics;
+    using UnityEditor;
+    using UnityEngine;
+    using Vector4 = UnityEngine.Vector4;
+
+    public class Vector4Drawer : BuiltInDrawer {
+        public Vector4Drawer(FriggProperty prop) : base(prop) {
+        }
+
+        public override void DrawLayout() {
+            var value  = this.GetTargetValue<Vector4>();
+            var result = EditorGUILayout.Vector4Field(this.property.Label, value);
+            this.UpdateAndCallNext(result);
+        }
+
+        public override void Draw(Rect rect) {
+            var value  = this.GetTargetValue<Vector4>();
+            var result = EditorGUI.Vector4Field(rect, this.property.Label, value);
+            rect.y += EditorGUIUtility.singleLineHeight;
+            this.UpdateAndCallNext(result, rect);
+        }
+
+        public override float GetHeight() => EditorGUIUtility.singleLineHeight;
+
+        public override bool IsVisible => true;
+        
+    }
+}
