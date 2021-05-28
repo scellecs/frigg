4ef90b6f4d4ad5187cf0dc0da7515186b6cd3173
Slavik Shudenko <shudenkoslavik@gmail.com>
2021-05-28 17:15:23 +0300

    Gradient drawer

---
+++ Editor/PropertyDrawers/BuiltIn/GradientDrawer.cs
@@ -0,0 +1,25 @@
+﻿namespace Frigg.Editor.BuiltIn {
+    using UnityEditor;
+    using UnityEngine;
+
+    public class GradientDrawer : BuiltInDrawer {
+        public GradientDrawer(FriggProperty prop) : base(prop) {
+        }
+
+        public override void DrawLayout() {
+            var value  = this.GetTargetValue<Gradient>();
+            var result = EditorGUILayout.GradientField(this.property.Label, value);
+            this.UpdateAndCallNext(result);
+        }
+
+        public override void Draw(Rect rect) {
+            var value  = this.GetTargetValue<Gradient>();
+            var result = EditorGUI.GradientField(rect, this.property.Label, value);
+            this.UpdateAndCallNext(result, rect);
+        }
+
+        public override float GetHeight() => throw new System.NotImplementedException();
+
+        public override bool IsVisible => true;
+    }
+}
