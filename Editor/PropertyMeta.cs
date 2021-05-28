e5e5dffa83b2202db8bfff4fadc8a12041b5292a
Slavik Shudenko <shudenkoslavik@gmail.com>
2021-05-28 17:20:56 +0300

    Some property meta information (type, member info)

---
+++ Editor/PropertyMeta.cs
@@ -0,0 +1,18 @@
+﻿namespace Frigg.Editor {
+    using System;
+    using System.Reflection;
+
+    public class PropertyMeta {
+        public Type   MemberType { get; set; }
+
+        public MemberInfo MemberInfo { get; set; }
+
+        public int    Order { get; set; }
+        
+        public string Name  { get; set; }
+        
+        public bool isArray;
+        public int  arraySize  = 0;
+        public int  arrayIndex = -1;
+    }
+}
