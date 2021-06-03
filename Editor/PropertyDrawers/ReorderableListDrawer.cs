0c6d43fc250572e62864b486ac9f9810866e923f
Slavik Shudenko <shudenkoslavik@gmail.com>
2021-06-03 12:55:53 +0300

    Expand elements & add & remove

--- Editor/PropertyDrawers/ReorderableListDrawer.cs
+++ Editor/PropertyDrawers/ReorderableListDrawer.cs
@@ -14,25 +14,30 @@
             var total = 0f;
             if (this.list.displayAdd || this.list.displayRemove) {
                 //button
-                total += EditorGUIUtility.singleLineHeight * 2;
+                if(this.property.IsExpanded)
+                   total += EditorGUIUtility.singleLineHeight * 1.5f;
             }
-
-            this.property.IsExpanded =  true;
-            total                    += FriggProperty.GetPropertyHeight(this.property) + 4f * this.list.list.Count; // paddings
-            return total;
+            
+            total += FriggProperty.GetPropertyHeight(this.property) + 4f * this.list.list.Count; // paddings
+            
+            if(this.property.IsExpanded)
+                return total;
+            
+            return EditorGUIUtility.singleLineHeight;
         }
 
         public override bool  IsVisible => true;
 
         public ReorderableListDrawer(FriggProperty prop) : base(prop) {
+            var elements = (IList) prop.PropertyValue.Value;
+            if (this.list == null)
+                this.list = new ReorderableList(elements, CoreUtilities.TryGetListElementType(elements.GetType()));
         }
 
         public override void Draw(Rect rect) {
-            var elements = (IList) CoreUtilities.GetTargetObject(this.property.ParentValue, this.PropertyMeta.MemberInfo);
-            if(this.list == null)
-                this.list = new ReorderableList(elements, CoreUtilities.TryGetListElementType(elements.GetType()));
-            
-            this.SetCallbacks(this.list, this.property);
+            rect.y += GuiUtilities.SPACE;
+            this.SetCallbacks(this.list, this.property, rect);
+            rect.y += EditorGUIUtility.singleLineHeight;
 
             rect.width -= EditorGUI.indentLevel * 15;
             rect.x     += EditorGUI.indentLevel * 15;
@@ -42,7 +47,7 @@
         }
         
         public override void DrawLayout() {
-            var elements = (IList) CoreUtilities.GetTargetObject(this.property.ParentValue, this.PropertyMeta.MemberInfo);
+            var elements = (IList) this.property.PropertyValue.Value;
             
             if(this.list == null)
                this.list = new ReorderableList(elements, CoreUtilities.TryGetListElementType(elements.GetType()));
@@ -53,11 +58,11 @@
                this.list.DoLayoutList();
         }
 
-        private void SetCallbacks(ReorderableList reorderableList, FriggProperty prop) {
+        private void SetCallbacks(ReorderableList reorderableList, FriggProperty prop, Rect rect = default) {
             this.list = reorderableList;
 
             this.property.Label.text = $"{this.property.NiceName} - {this.property.MetaInfo.arraySize} elements.";
-            this.property.IsExpanded = GuiUtilities.FoldoutToggle(this.property);
+            this.property.IsExpanded = GuiUtilities.FoldoutToggle(this.property, rect);
             
             this.list.draggable      = this.list.displayAdd = this.list.displayRemove = true;
             this.list.headerHeight   = 1;
@@ -96,6 +101,13 @@
                 for (var i = 0; i < copy.Count; i++) {
                     this.list.list[i] = copy[i];
                 }
+
+                if (!string.IsNullOrEmpty(prop.UnityPath)) {
+                    var sp = prop.PropertyTree.SerializedObject.FindProperty(prop.UnityPath);
+                    sp.arraySize++;
+                }
+
+                this.property.MetaInfo.arraySize++;
             };
 
             this.list.onRemoveCallback = delegate {
@@ -108,6 +120,14 @@
 
                 for (var i = this.list.index; i < newLength; i++)
                     this.list.list[i] = copy[i + 1];
+                
+                if (!string.IsNullOrEmpty(prop.GetArrayElementAtIndex(this.list.index).UnityPath)) {
+                    var sp = prop.PropertyTree.SerializedObject.FindProperty(prop.Name);
+                    sp.DeleteArrayElementAtIndex(this.list.index);
+                    sp.arraySize--;
+                }
+                
+                this.property.MetaInfo.arraySize--;
             };
 
             this.list.elementHeightCallback = index => {
