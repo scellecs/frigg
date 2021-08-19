19dc50f6221512c471b0d3a704b01a1b8ec9024a
Slavik Shudenko <shudenkoslavik@gmail.com>
2021-08-19 13:19:52 +0300

    Add some extension methods

---
+++ Editor/Utils/PropertyExtensions.cs
@@ -0,0 +1,13 @@
+﻿namespace Packages.Frigg.Editor.Utils {
+    using global::Frigg.Editor;
+    using global::Frigg.Utils;
+
+    public static class PropertyExtensions {
+        public static object GetValue(this FriggProperty property) 
+            => CoreUtilities.GetTargetValue(property.ParentValue, property.MetaInfo.MemberInfo);
+
+        public static void SetValue(this FriggProperty property, object value) {
+            CoreUtilities.SetTargetValue(property, property.ParentValue, property.MetaInfo.MemberInfo, value);
+        }
+    }
+}
