fcf93a0767f0f6d411c31c58554e1b05ad981d47
Slavik Shudenko <shudenkoslavik@gmail.com>
2021-10-01 03:48:22 +0300

    Impl layout element

---
+++ Editor/Layouts/LayoutElement.cs
@@ -0,0 +1,18 @@
+﻿namespace Frigg.Editor.Layouts {
+    using System.Drawing;
+    using Groups;
+    using UnityEngine;
+
+    public class LayoutElement {
+        public FriggProperty      property;
+        public BaseGroupAttribute attribute;
+
+        public float xOffset;
+        public float yOffset;
+
+        public LayoutElement(FriggProperty property, BaseGroupAttribute attribute) {
+            this.property  = property;
+            this.attribute = attribute;
+        }
+    }
+}
