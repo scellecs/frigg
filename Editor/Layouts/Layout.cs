55c47f61719690ed4b87486749d2fcc5a8e3eab3
Slavik Shudenko <shudenkoslavik@gmail.com>
2021-10-12 19:27:08 +0300

    Add some docs and properties

--- Editor/Layouts/Layout.cs
+++ Editor/Layouts/Layout.cs
@@ -5,14 +5,34 @@
     using UnityEngine;
 
     public abstract class Layout {
-        public float totalHeight => this.GetTotalHeight();
+        protected const float HORIZONTAL_SPACING = 5.0f;
+        
+        public bool IsListMember { get; set; }
 
+        /// <summary>
+        /// Get total height of target layout.
+        /// </summary>
+        public float TotalHeight => this.GetTotalHeight();
+
+        /// <summary>
+        /// Path of this layout group.
+        /// </summary>
         public string layoutPath;
+        
+        /// <summary>
+        /// Group name, depends on Attribute's 'Name" value.
+        /// </summary>
         public string layoutName;
 
-        //To dictionary or hashset
-        protected List<LayoutElement> layoutElements = new List<LayoutElement>();
+        /// <summary>
+        /// List of layout elements of this Layout Group.
+        /// </summary>
+        protected readonly List<LayoutElement> layoutElements = new List<LayoutElement>();
 
+        /// <summary>
+        /// Add a property to target layout.
+        /// </summary>
+        /// <param name="property">Target property to add.</param>
         public virtual void Add(FriggProperty property) {
             var attribute = property.TryGetFixedAttribute<BaseGroupAttribute>();
             this.layoutName = attribute.GroupName;
@@ -22,31 +42,31 @@
             this.layoutElements.Add(new LayoutElement(property, attribute));
         }
 
-        public virtual void Remove() {
-            
-        }
-
         protected abstract void Recalculate();
 
-            private float GetTotalHeight() {
-            var height = 0;
+        private float GetTotalHeight() {
+            var height = 0f;
             foreach (var element in this.layoutElements) {
-                
+                var last = FriggProperty.GetPropertyHeight(element.property);
+                if (last > height) {
+                    height = last;
+                }
             }
-
             return height;
         }
 
         /// <summary>
         /// Draws whole group in the provided position.
         /// </summary>
-        /// <param name="rect">Start position for this group</param>
+        /// <param name="rect">Start position for this group.</param>
         public void Draw(Rect rect = default) {
             this.Recalculate();
+            
             foreach (var element in this.layoutElements) {
+                rect.width =  element.width;
                 element.property.Draw(rect);
-                rect.x      += element.xOffset;
-                rect.width -= element.xOffset;
+                rect.y += element.yOffset;
+                rect.x += element.width + HORIZONTAL_SPACING;
             }
         }
     }
