0658cbc9d20f1a7a69939b32837568eb2523eda3
Slavik Shudenko <shudenkoslavik@gmail.com>
2021-10-23 14:49:57 +0300

    Store only actual value

--- Editor/PropertyValue.cs
+++ Editor/PropertyValue.cs
@@ -25,32 +25,15 @@
     public class PropertyValue<T> {
         public  PropertyMeta MetaInfo { get; set; }
 
-        public GetterSetter<T> actual;
-        public GetterSetter<T> parent;
+        private readonly GetterSetter<T> actual;
 
         public PropertyValue(object parent, object value, PropertyMeta metaInfo) {
-            this.parent = new GetterSetter<T>(() => (T) parent, x => parent = x);
             this.actual = new GetterSetter<T>(() => (T) value, x => value = x);
 
             this.MetaInfo = metaInfo;
         }
-        
-        /*private object       actual;
-        private object       parent;
 
-        public object ActualValue {
-            get => this.actual;
-            set => this.actual = value;
-        }
-
-        public object Parent {
-            get => this.parent;
-            set => this.parent = value;
-        }*/
-
-        /*public PropertyValue(object parent = null, object value = null) {
-            this.parent = parent;
-            this.actual = value;
-        }*/
+        public T Get()        => this.actual.Value;
+        public void   Set(T value) => this.actual.Value = value;
     }
 }
